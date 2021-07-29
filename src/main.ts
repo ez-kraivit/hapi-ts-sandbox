9e868ddee1694af0960a492daaf946491ad3e247
Kraivit Mongkhonsakunrit <kraivit.m@edvisory.co.th>
2021-07-29 10:00:59 +0700

    Update : server & main

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,2 @@
-import initServer from './server';
-const init = async function (): Promise<void>{
-    let server = await initServer();
-    await server.start();
-    console.info(`Server runing on ${server.info.port}`);
-}
-init()
+import { init, start } from "./server";
+init().then(() => start());
--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,18 @@
 import * as Hapi from '@hapi/hapi';
+import { Server } from "@hapi/hapi";
 import { SO } from './datatypes/mapperInterface'
 import hapiarrayPaths from './hapiarrayPaths'
+export let server: Server;
 
-const initServer = async(_options?:SO) => {
-    const server = new Hapi.Server({
+export const init =  async function(_options?:SO): Promise<Server> {
+    server = new Hapi.Server({
         port:5070
     }) 
     await server.register(hapiarrayPaths);
     return server
 }
 
-export default initServer
-
+export const start = async function (): Promise<void>{
+    console.info(`Server runing on ${server.info.port}`);
+    await server.start();
+}
